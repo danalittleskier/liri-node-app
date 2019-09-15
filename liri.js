0070d9cc3e7e353216b5626454909c4e49009b41
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-09-14 18:28:20 -0600

    Created case for grabbing input from random.txt file

--- liri.js
+++ liri.js
@@ -3,12 +3,15 @@ var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var moment = require('moment');
-//moment().format();
+var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 
 var nodeArgs = process.argv;
 var queryRequest = process.argv[2];
+
+//store arguments to use in all functions
+var searchtext = grabArguments(nodeArgs);
 
 //concatinate arguments with a '+'
 function grabArguments(nodeArgs) {
@@ -25,18 +28,42 @@ function grabArguments(nodeArgs) {
     return arguments;
 }
 
-var searchtext = grabArguments(nodeArgs);
+//check if reading input from a file
+if(queryRequest === "do-what-it-says" || queryRequest === undefined){
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+          return console.log(error);
+        }
+        var dataArr = data.split(",");
+        queryRequest = dataArr[0];
+        searchtext = grabArguments(dataArr[1]);
+        callAPI(queryRequest);
+      
+      });
+} else {
+    callAPI(queryRequest);
+}
 
-var bandAPI = "https://rest.bandsintown.com/artists/" + searchtext + "/events?app_id=codingbootcamp";
-var movieAPI = "http://www.omdbapi.com/?t=" + searchtext + "&y=&plot=short&apikey=trilogy";
 
+
+//call API depending on the query request
 function callAPI(queryRequest) {
-
     if (queryRequest === "spotify-this-song") {
+        if(searchtext === ""){
+            searchtext = "The Sign";
+        }
         spotify
             .search({ type: 'track', limit: 2, query: searchtext })
             .then(function (response) {
-                console.log(response.tracks.items[1]);
+                console.log("-------Song------");
+                //console.log(response.tracks.items[0]);
+                console.log("Song name:  " +response.tracks.items[0].name);
+                console.log("Album:  " +response.tracks.items[0].album.name);
+                console.log("Preview:  " +response.tracks.items[0].preview_url);
+                console.log("Artist:  " +response.tracks.items[0].artists[0].name);
+
+                console.log("----------------------");
+
             })
             .catch(function (err) {
                 console.log(err);
@@ -44,6 +71,11 @@ function callAPI(queryRequest) {
     }
     else {
         if (queryRequest === "concert-this") {
+            if(searchtext === ""){
+                searchtext = "Dave Matthews Band";
+            }
+
+            var bandAPI = "https://rest.bandsintown.com/artists/" + searchtext + "/events?app_id=codingbootcamp";
             axios.get(bandAPI).then(
                 function (response) {
                     console.log("-------Bands------");
@@ -53,9 +85,15 @@ function callAPI(queryRequest) {
                     console.log("City:  " + response.data[0].venue.city + " State " + response.data[0].venue.region);
                     console.log("Date:  " + moment(response.data[0].datetime).format('MM/DD/YYYY'));
 
+                    console.log("----------------------");
+
                 }).catch(err);
         }
         if (queryRequest === "movie-this") {
+            if(searchtext === ""){
+                searchtext = "Mr. Nobody";
+            }
+            var movieAPI = "http://www.omdbapi.com/?t=" + searchtext + "&y=&plot=short&apikey=trilogy";
             axios.get(movieAPI).then(
                 function (response) {
                     //console.log(response.data);
@@ -69,6 +107,7 @@ function callAPI(queryRequest) {
                     console.log("Actors:  " + response.data.Actors);
                     console.log("Plot:  " + response.data.Plot);
 
+                    console.log("----------------------");
 
                 }).catch(err);
         }
@@ -94,4 +133,6 @@ function callAPI(queryRequest) {
     };
 }
 
-callAPI(queryRequest);
+
+
+//callAPI(queryRequest);
